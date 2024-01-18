aca964f6a880f4bf83cabbacdf2d4fd2e3134996
massmr <marcellin.massimo@gmail.com>
2024-01-18 19:07:51 +0100

    add a todo input

--- src/MainWrapper/CardsContainer/CardsContainer.jsx
+++ src/MainWrapper/CardsContainer/CardsContainer.jsx
@@ -1,8 +1,17 @@
 import './CardsContainer.css'
+import React, { useState } from 'react';
 
 const CardsContainer = ({ tasks, activeCategory }) => {
-  console.log(activeCategory)
-  
+
+  const [isFocused, setIsFocused] = React.useState(false);
+
+  const handleInputFocus = () => {
+    setIsFocused(true);
+  };
+  const handleInputBlur = () => {
+    setIsFocused(false);
+  };
+
   //takes all tasks and return a new arr filtered with item.done
   const filterTasks = (tasks, category) => {
     return tasks.filter((item) => {
@@ -17,25 +26,40 @@ const CardsContainer = ({ tasks, activeCategory }) => {
     <section className="cards-container">
       
       <div className="cards-wrapper">
-
+        
         <div className="card todo-card" value="todo">
-          <ul>
-            {todoTasks.map((item, index) => (
-              <li key={index}>
-                <span>{item.task}</span>
-              </li>
-            ))}
-          </ul>
+          <div className="items-wrapper">
+            <ul>
+              {todoTasks.map((item, index) => (
+                <li key={index}>
+                  <span>{item.task}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div 
+            className={`input-wrapper ${isFocused ? 'input-wrapper-focus' : ''}`}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+          >
+            <label htmlFor="todo-input">Add a todo</label>
+            <div className="textZone-wrapper">
+              <input type="text" id="todo-input" name="todo-input"/>
+              <input type="submit" value="Post" id="submit-todo-input"/>
+            </div>
+          </div>
         </div>
         
         <div className="card done-card" value="done">
-          <ul>
-            {doneTasks.map((item, index) => (
-              <li key={index}>
-                <span>{item.task}</span>
-              </li>
-            ))}
-          </ul>
+          <div className="items-wrapper">
+            <ul>
+              {doneTasks.map((item, index) => (
+                <li key={index}>
+                  <span>{item.task}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       
       </div>
